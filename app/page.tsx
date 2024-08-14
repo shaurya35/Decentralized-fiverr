91080825a48351c01fbe5dbb136716c283911757
Shaurya Jha <shauryajha35@gmail.com>
2024-08-15 04:15:10 +0530

    Add Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,100 @@
-// max width wrapper 
+// max width wrapper
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
-// links 
+// links
 import Link from "next/link";
 
 // components
 import MainTiles from "@/components/MainTiles";
 
+// icons
+import {
+  LaptopMinimal,
+  Palette,
+  CircleDollarSign,
+  Languages,
+  Video,
+  Brain,
+  Music,
+  Handshake,
+} from "lucide-react";
+
+// logic for icons
+const icons = [
+  {
+    Icon: LaptopMinimal,
+    content: "Programming & Tech",
+  },
+  {
+    Icon: Palette,
+    content: "Graphic & Design",
+  },
+  {
+    Icon: CircleDollarSign,
+    content: "Digital Marketing",
+  },
+  {
+    Icon: Languages,
+    content: "Writing & Translation",
+  },
+  {
+    Icon: Video,
+    content: "Video & Animation",
+  },
+  {
+    Icon: Brain,
+    content: "AI Service",
+  },
+  {
+    Icon: Music,
+    content: "Music & Audio",
+  },
+  {
+    Icon: Handshake,
+    content: "Business",
+  },
+];
+
 export default function Home() {
   return (
     <>
-    {/* app_main */}
-    <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex flex-col items-center mex-w-3xl sm:py-32 ">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Find the right freelance <br /> sevice,{" "}
-          <span className="text-blue-600">right away</span>.
-        </h1>
-        <p className="mt-4 text-lg max-w-prose font-semibold text-muted-foreground sm:mt-6">
-          Welcome to Decentralized Fiverr, the <br /> next-generation freelance
-          marketplace <br /> built on the Solana blockchain.
-        </p>
-        <div className="flex flex-col sm:flex-row gap-3 mt-6">
-          <Link href="/">
-            <button
-              className="text-lg font-semibold text-white border-2 rounded-lg bg-blue-600 py-2
+      {/* app_main */}
+      <MaxWidthWrapper>
+        <div className="pt-24 mx-auto text-center flex flex-col items-center mex-w-3xl sm:pt-52">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Find the right freelance <br /> sevice,{" "}
+            <span className="text-blue-600">right away</span>.
+          </h1>
+          <p className="mt-4 text-lg max-w-prose font-semibold text-muted-foreground sm:mt-6">
+            Welcome to Decentralized Fiverr, the <br /> next-generation
+            freelance marketplace <br /> built on the Solana blockchain.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-3 mt-6">
+            <Link href="/">
+              <button
+                className="text-lg font-semibold text-white border-2 rounded-lg bg-blue-600 py-2
             px-4"
-            >
-              Get Started
+              >
+                Get Started
+              </button>
+            </Link>
+            <button className="text-lg font-semibold border-2 rounded-lg bg-slate-10 py-2 px-4">
+              Explore &rarr;
             </button>
-          </Link>
-          <button className="text-lg font-semibold border-2 rounded-lg bg-slate-10 py-2 px-4">Explore &rarr;</button>
-        </div>
-      </div>
-    </MaxWidthWrapper>
-
-    {/* app_section */}
-    <section className="border-t border-gray-200 bg-gray-50">
-      <MaxWidthWrapper className="py-20">
-        <div className="flex">
-          <MainTiles/>
+          </div>
         </div>
       </MaxWidthWrapper>
-    </section>
+
+      {/* app_section */}
+      <section className="sm:pt-24 pt-14">
+        <MaxWidthWrapper>
+          <div className="flex flex-row gap-3 flex-wrap justify-center">
+          {icons.map(({ Icon, content }) => (
+              <MainTiles key={content} Icon={Icon} content={content} />
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </section>
     </>
   );
 }
--- components/MainTiles.tsx
+++ components/MainTiles.tsx
@@ -1,7 +1,17 @@
-export default function MainTiles(){
-    return(
-        <>
-            hi there new tile 
-        </>
-    )
+import React from 'react';
+
+interface MainTilesProps {
+  Icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  content: string;
 }
+
+const MainTiles: React.FC<MainTilesProps> = ({ Icon, content }) => {
+  return (
+    <div className="h-32 w-32 rounded-2xl flex flex-col justify-around p-3 border-2 border-gray-200 cursor-pointer shadow-md">
+      <Icon className="w-8 h-8 mx-auto" />
+      <div className="text-center font-semibold">{content}</div>
+    </div>
+  );
+};
+
+export default MainTiles;
